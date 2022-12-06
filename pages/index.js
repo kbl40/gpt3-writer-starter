3d52dec50028af4a28d9e9f7d74d6a80d82b5108
Kyle B. Lackinger <67078189+kbl40@users.noreply.github.com>
2022-12-06 01:17:16 -0500

    added useState

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const onUserChangedText = (event) => {
+    // console.log(event.target.value);
+    setUserInput(event.target.value);
+  }
+
   return (
     <div className="root">
       <Head>
@@ -15,6 +23,21 @@ const Home = () => {
           </div>
           <div className="header-subtitle">
             <h2>insert your subtitle here</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea 
+            placeholder="start typing here" 
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText} 
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
